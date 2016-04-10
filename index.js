46272d83e30b4be8d21e2d89cba26e640d49ccff
okovko <okovko@gmail.com>
2016-04-09 22:46:48 -0500

    added server code

---
+++ index.js
@@ -0,0 +1,28 @@
+var app = require ( 'express' ) ();
+var http = require ( 'htpp' ).Server ( app );
+var io = require ( 'socket.io' ) ( http );
+
+var cookies = 100;
+
+app.get ( '/', function ( req, res ) {
+	res.sendFile ( __dirname + '/index.html' );
+});
+
+io.on ( 'connection', function ( socket ) {
+	socket.on ( 'request cookie', function (  ) {
+		if ( cookies > 0 ) {
+			cookies--;
+			io.emit ( 'update', { cookies: cookies } );
+		} else {
+			io.emit ( 'update', { cookies: cookies, error: 'out of cookies' } );
+		}
+	} );
+} );
+
+setInterval ( function () {
+	io.emit ( 'update', { cookies: cookies } );
+}, delay = 1000 );
+
+http.listen ( 3000, function () {
+	console.log ( 'server on, port 3000' );
+} );
