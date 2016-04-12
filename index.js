72738a50ac9d965a7052adf395b58a1bd3711a31
Stevan Dedovic <stevan@dedovic.com>
2016-04-12 18:30:10 -0500

    add player object

--- index.js
+++ index.js
@@ -2,27 +2,32 @@ var app = require ( 'express' ) ();
 var http = require ( 'http' ).Server ( app );
 var io = require ( 'socket.io' ) ( http );
 
+var Player = require('./models/player');
+
 var cookies = 100;
+var players = [];
 
 app.get ( '/', function ( req, res ) {
-	res.sendFile ( __dirname + '/index.html' );
+	  res.sendFile ( __dirname + '/index.html' );
 });
 
 io.on ( 'connection', function ( socket ) {
-	socket.on ( 'request cookie', function (  ) {
-		if ( cookies > 0 ) {
-			cookies--;
-			io.emit ( 'update', { cookies: cookies } );
-		} else {
-			io.emit ( 'update', { cookies: cookies, error: 'out of cookies' } );
-		}
-	} );
+    var player = new Player("Player: " + players.length, socket);
+    socket.on ( 'request cookie', function (  ) {
+		    if ( cookies > 0 ) {
+			      cookies--;
+            player.cookies++;
+			      io.emit ( 'update-player', { cookies: player.cookies } );
+		    } else {
+			      io.emit ( 'update-player', { cookies: player.cookies, error: 'out of cookies' } );
+		    }
+	  } );
 } );
 
 setInterval ( function () {
-	io.emit ( 'update', { cookies: cookies } );
+	  io.emit ( 'update-world', { cookies: cookies } );
 }, delay = 1000 );
 
 http.listen ( 3000, function () {
-	console.log ( 'server on, port 3000' );
+	  console.log ( 'server on, port 3000' );
 } );
